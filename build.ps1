24d51ace034456b785a1ef68177aa4f56334074b
Olivier Lefebvre <olivier.lefebvre@live.com>
2021-02-16 14:30:09 +0100

    ci: setup

---
+++ build.ps1
@@ -0,0 +1,52 @@
+$result = 0
+
+if ($isLinux) {
+    gci -rec `
+    | ? { $_.Name -like "*.IntegrationTest.csproj" `
+           -Or $_.Name -like "*.Test.csproj" `
+         } `
+    | % { 
+        cd $_.DirectoryName
+        dotnet test
+    
+        if ($LASTEXITCODE -ne 0) {
+            $result = $LASTEXITCODE
+        }
+    }
+} else {
+    $prNumber = $env:APPVEYOR_PULL_REQUEST_NUMBER
+    if ($prNumber) {
+        $prArgs = "-d:sonar.pullrequest.key=$prNumber"
+    } elseif ($env:APPVEYOR_REPO_BRANCH) {
+        $prArgs = "-d:sonar.branch.name=$env:APPVEYOR_REPO_BRANCH"
+    }
+
+    dotnet sonarscanner begin /k:aguacongas_Identity.Redis -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=$env:sonarqube -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:version
+
+    dotnet build -c Release
+
+    Get-ChildItem -rec `
+    | Where-Object { $_.Name -like "*.IntegrationTest.csproj" `
+           -Or $_.Name -like "*.Test.csproj" `
+         } `
+    | ForEach-Object { 
+        &('dotnet') ('test', $_.FullName, '--logger', "trx;LogFileName=$_.trx", '--no-build', '-c', 'Release', '--collect:"XPlat Code Coverage"')    
+        if ($LASTEXITCODE -ne 0) {
+            $result = $LASTEXITCODE
+        }
+      }
+
+    $merge = ""
+    Get-ChildItem -rec `
+    | Where-Object { $_.Name -like "coverage.cobertura.xml" } `
+    | ForEach-Object { 
+        $path = $_.FullName
+        $merge = "$merge;$path"
+    }
+    Write-Host $merge
+    ReportGenerator\tools\netcoreapp3.0\ReportGenerator.exe "-reports:$merge" "-targetdir:coverage" "-reporttypes:SonarQube"
+    
+    dotnet sonarscanner end -d:sonar.login=$env:sonarqube
+}
+exit $result
+  
--- test/Aguacongas.Identity.RavenDb.IntegrationTest/RavenDbTestFixture.cs
+++ test/Aguacongas.Identity.RavenDb.IntegrationTest/RavenDbTestFixture.cs
@@ -9,24 +9,13 @@ namespace Aguacongas.Identity.RavenDb.IntegrationTest
 {
     public class RavenDbTestFixture
     {
-        IDocumentStore _store;
+        readonly IDocumentStore _store;
         public IDocumentStore Store => _store;
 
 
         public RavenDbTestFixture()
         {
              _store = new RavenDbTestDriverWrapper().GetDocumentStore();
-            //_store = new DocumentStore
-            //{
-            //    Urls = new[]
-            //    {
-            //        "https://a.ravendb.local",
-            //        "https://b.ravendb.local",
-            //        "https://c.ravendb.local"
-            //    },
-            //    Certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(@"C:\Projects\Perso\helm-charts\cluster.admin.cert\cluster.admin.client.certificate.pfx", "p@$$w0rd"),
-            //    Database = "Test"
-            //}.Initialize();
         }
 
         class RavenDbTestDriverWrapper : RavenTestDriver
